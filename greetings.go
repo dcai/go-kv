5bf555f6397c55f6be70ab92a7352faad70a3d06
Dongsheng Cai <d@tux.im>
2024-03-20 00:59:38 +1100

    greetings

--- greetings.go
+++ greetings.go
@@ -1,15 +1,38 @@
 package main
 
 import "fmt"
-import "rsc.io/quote"
 import "errors"
+import "rsc.io/quote"
+import "math/rand"
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
 
 func Hello(name string) (string, error) {
 	if name == "" {
-        return "", errors.New("ERROR: empty name")
+		return "", errors.New("ERROR: empty name")
 
 	}
 	fmt.Println(quote.Go())
 
-	return fmt.Sprintf("hello, %s", name), nil
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
 }
